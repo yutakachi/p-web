4315cb165895e51ce05a0107da9bbe9f091fd094
uchi <uchi74@kpb.biglobe.ne.jp>
2023-07-29 18:10:04 +0900

    jsonやり取り完成

--- static/json.js
+++ static/json.js
@@ -1,8 +1,20 @@
+const jsonFnc = async (ev) => {
+    
+    const response = await fetch("jjjson", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"abc": 123})
+    })
+    
+    const j = await response.json();
+    
+    const ret = document.getElementById("result");
+    ret.innerHTML = JSON.stringify(j);
+
+}
+
 const jb = document.getElementById("jsontest");
 
 jb.addEventListener('click', jsonFnc);
-
-const jsonFnc = async (ev) => {
-    await fetch()
-
-}
--- web.py
+++ web.py
@@ -1,10 +1,21 @@
-from flask import Flask, redirect ,request,render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods = ['GET','POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/jjjson', methods = ['GET','POST'])
+def jjjson():
+    # json をもらってきてデコードしたい
+    param = json.loads(request.data.decode('utf-8'))
+    col1 = param.get('abc')
+    print(col1)     # 123
+    # return {"result": "OK"}
+    # json エンコードして返却する
+    return json.dumps(param).encode('utf-8')
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=8888)
